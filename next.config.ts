ce7db11056d0159832169cb166c9a8d6f5e1de84
agusttd <leandrofilun@gmail.com>
2025-12-13 00:13:17 -0300

    Se arregló next.config.ts para Nextjs 15

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Ignora los errores de ESLint (como variables no usadas o 'any') durante el build
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Ignora los errores de tipos de TypeScript durante el build
     ignoreBuildErrors: true,
